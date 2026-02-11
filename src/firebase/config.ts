4db6293dce55c97c798e65b61f30fb23a6061992
Mendex Technologies <mendextechnologies@gmail.com>
2026-02-11 14:42:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9237984229-c8ef8",
+  "appId": "1:844865964974:web:3284234cf4eb1a880d963f",
+  "apiKey": "AIzaSyAhgXIX0i4UFmt1KRiqPtbDE7gW5MdgV2A",
+  "authDomain": "studio-9237984229-c8ef8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "844865964974"
+};
